084e64f57e3e6dfb3ef152459afb70eacc3cede8
soohyun-lee <soohyun527@gmail.com>
2020-09-07 18:15:05 +0900

    soohyunlee 초기 세팅 완료

---
+++ soohyunlee/products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+#class Prodcut(models.Model):
+  #  name        = models.CharField(max_length=100)
+  #  description = models.TextField(max_length=500)
+  #  category_id =
+  #  nutrition_id =
+  #  class Meta:
+  #      db_table='product_list'
+# Create your models here.
