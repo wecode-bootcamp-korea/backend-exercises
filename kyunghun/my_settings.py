a269b0bda347ab727131d0582551a0a13857a016
Kang-Kyunghun <korea271150@gmail.com>
2020-09-07 18:28:14 +0900

    project setting

---
+++ kyunghun/my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET = {
+        'secret': '(!w(*h87zw5$c3*^$kt-1+p&cv7j8*c^g#!%$w(zef@k!@=-4o',
+}
---
+++ kyunghun/starbucks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+#from .views  import SignUpView, LogInView
+
+urlpatterns = [
+    #path('/signup', SignUpView.as_view()),
+]
